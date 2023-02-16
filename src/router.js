153e159cb761599bd73decaca61fbb95ac150bfc
samwhitford11 <samantha.fichte98@gmail.com>
2023-02-15 19:07:46 -0700

    fix router.js file

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import {createBrowserRouter, createRoutesFromElements, Route, Routes} from "react-router-dom"
+import {createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom"
 import App from "./App"
 import Index from "./pages/Index"
 import Show from "./pages/Show"
